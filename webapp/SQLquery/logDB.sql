e73c3471e6d43cab372a185d03f625efdc3fb7ae
MikhailGusarov <vgg639@gmail.com>
2019-06-15 16:44:00 +0300

    Запросы 1

---
+++ webapp/SQLquery/logDB.sql
@@ -0,0 +1,13 @@
+create database vsearchlogDB;
+CREATE USER 'vsearch' IDENTIFIED BY 'vsearchpasswd';
+GRANT ALL ON vsearchlogDB.* TO 'vsearch';
+use vsearchlogDB;
+create table log (
+    id int auto_increment primary key,
+    ts timestamp default current_timestamp,
+    phrase varchar(128) not null,
+    letters varchar(32) not null,
+    ip varchar(16) not null,
+    browser_string varchar(256) not null,
+    results varchar(64) not null );
+

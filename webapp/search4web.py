186f7de9761f73c61ff8a5d2156ba4aadb5e038d
Mikhail Gusarov <vgg639@gmail.com>
2019-06-18 15:50:03 +0300

    Добавил обработку исключений. Наверное...

--- webapp/search4web.py
+++ webapp/search4web.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, escape
+from flask import Flask, render_template, request, escape, session
 from vsearch import search4letters
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
+from checker import check_logged_in
 
 app = Flask(__name__)
+
+app.secret_key = 'Yuef'
 
 app.config['dbconfig'] = {'host' : '127.0.0.1',
                 'user' : 'vsearch',
@@ -27,12 +30,16 @@ def do_search() -> str:
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
-    log_request(request, results)
+    try:
+        log_request(request, results)
+    except Exception as err:
+        print('Доигрался чёрт ушастый!', str(err))
     return render_template('results.html',
                            the_title=title,
                            the_phrase=phrase,
                            the_letters=letters,
                            the_results=results,)
+
 
 @app.route('/')
 @app.route('/entry')
@@ -41,15 +48,36 @@ def entry_page() -> 'html':
                            the_title='Welcome to search4letters on the web!')
 
 @app.route('/viewlog')
+@check_logged_in
 def view_the_log() -> 'html':
-    with UseDatabase(app.config['dbconfig'] ) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, results from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-    return render_template('viewlog.html',
+    try:
+        with UseDatabase(app.config['dbconfig'] ) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, results from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        return render_template('viewlog.html',
                            the_title='Log search4wed',
                            the_row_titles=('Phrase', 'Letters', 'IP', 'Browser', 'Results'),
                            the_data = contents,)
+    except ConnectionError as err:
+        print('БД включил? Error: ', str(err))
+    except CredentialsError as err:
+        print('неправильный логин или пароль. Error: ', str(err))
+    except SQLError as err:
+        print('Ошибка в запросе. Ошибка: ',str(err))
+    except Exception as err:
+        print('Error: ',str(err))
+    return 'Error'
+
+@app.route('/login')
+def login() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in.'
+
+@app.route('/logout')
+def logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out.'
     
 
 app.run(debug=True)

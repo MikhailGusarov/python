3e4395346ca4364ef53b9bc05a4112b89da9971f
MikhailGusarov <vgg639@gmail.com>
2019-06-18 23:14:03 +0300

    Параллель

--- webapp/search4web.py
+++ webapp/search4web.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, escape, session
+from flask import Flask, render_template, request, escape, session, copy_current_request_context
 from vsearch import search4letters
 from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from checker import check_logged_in
+from threading import Thread
+from time import sleep
 
 app = Flask(__name__)
 
@@ -12,17 +14,6 @@ app.config['dbconfig'] = {'host' : '127.0.0.1',
                 'password' : 'vsearchpasswd',
                 'database' : 'vsearchlogDB'}
 
-def log_request(req: 'flask_request',res: str) -> None:
-    with UseDatabase(app.config['dbconfig'] ) as cursor:
-        _SQL = """insert into log
-            (phrase, letters, ip, browser_string, results)
-            values
-            (%s,%s,%s,%s,%s)"""
-        cursor.execute(_SQL,(req.form['phrase'],
-                         req.form['letters'],
-                         req.remote_addr,
-                         req.user_agent.browser,
-                         res,))
 
 @app.route('/search4',methods=['POST'])
 def do_search() -> str:
@@ -30,8 +21,24 @@ def do_search() -> str:
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
+
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        """Логирует запрос в БД"""
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            sleep(15)
+            _SQL = """insert into log
+                (phrase, letters, ip, browser_string, results)
+                values
+                (%s,%s,%s,%s,%s)"""
+            cursor.execute(_SQL, (req.form['phrase'],
+                                  req.form['letters'],
+                                  req.remote_addr,
+                                  req.user_agent.browser,
+                                  res,))
     try:
-        log_request(request, results)
+        log_thread = Thread(target=log_request, args=(request, results))
+        log_thread.start()
     except Exception as err:
         print('Доигрался чёрт ушастый!', str(err))
     return render_template('results.html',
